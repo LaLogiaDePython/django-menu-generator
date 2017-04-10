d8dd01063c45428c93778165618aa8b1c931425e
Milton Lenis <milton.lenis@correounivalle.edu.co>
2017-04-10 00:55:11 -0500

    Update __init__.py

--- menu_generator/__init__.py
+++ menu_generator/__init__.py
@@ -1,5 +1,5 @@
-__author__ = 'Val Neekman @ Neekware Inc.'
-__description__ = 'A menu generating application for Django.'
+__author__ = 'Milton Lenis @ Rady Consultores'
+__description__ = 'A straightforward menu generator for Django'
 __version__ = '1.0.0'
 
 default_app_config = 'menu_generator.apps.MenuAppConfig'
